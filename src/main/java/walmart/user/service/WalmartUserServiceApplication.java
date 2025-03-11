389255cc072c3d034005245276e7496c1155a83c
Chandrakanta Behera <chandrakantab@maveric-systems.com>
2025-03-11 20:28:24 +0530

    walmart-user-service repo first commit

---
+++ src/main/java/walmart/user/service/WalmartUserServiceApplication.java
@@ -0,0 +1,13 @@
+package walmart.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalmartUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalmartUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/walmart/user/service/WalmartUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package walmart.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalmartUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
